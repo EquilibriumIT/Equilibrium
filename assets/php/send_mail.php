39f2622ff0618aa98d0600668b5c3d7f34d0586b
stuartnuttall84 <stuart.nuttall@equilibriumict.com>
2019-07-24 14:32:23 -0700

    new changes July 24 - changed contact us page again - changed a line

--- assets/php/send_mail.php
+++ assets/php/send_mail.php
@@ -8,76 +8,65 @@ function died($error) {
     echo "Please go back to basics and fix these errors.<br /><br />";
     exit();
 }
-<!-- Note :
-   - You can modify the font style and form style to suit your website.
-   - Code lines with comments ���Do not remove this code���  are required for the form to work properly, make sure that you do not remove these lines of code.
-   - The Mandatory check script can modified as to suit your business needs.
-   - It is important that you test the modified form before going live.-->
-<div id='crmWebToEntityForm' style='width:600px;margin:auto;'>
-   <META HTTP-EQUIV ='content-type' CONTENT='text/html;charset=UTF-8'>
-   <form action='https://crm.zoho.com/crm/WebToLeadForm' name=WebToLeads3978741000000251006 method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory3978741000000251006()' accept-charset='UTF-8'>
- <input type='text' style='display:none;' name='xnQsjsdp' value='409f5f5f57ecb0e567902c2da462f9f3bdebdf45daecdbff6851ba9bb4bbd4a6'></input>
- <input type='hidden' name='zc_gad' id='zc_gad' value=''></input>
- <input type='text' style='display:none;' name='xmIwtLD' value='c9f44193f145f021ad7042be6dbe76eb2298d3513d0329ec8c52da14e67ca522'></input>
- <input type='text'  style='display:none;' name='actionType' value='TGVhZHM='></input>
- <input type='text' style='display:none;' name='returnURL' value='https://www.equilibriumict.com' > </input><br></br>
-	 <!-- Do not remove this code. -->
-	<style>
-		#crmWebToEntityForm tr , #crmWebToEntityForm td {
-			padding:6px;
-			border-spacing:0px;
-			border-width:0px;
-			}
-	</style>
-	<table style='width:600px;background-color:#ffffff;background-color:white;color:black'><tr><td colspan='2' align='left' style='color:black;font-family:Arial;font-size:14px;word-break: break-word;'><strong>Webform1</strong></td></tr> <br></br><tr><td  style='word-break: break-word;text-align:left;font-size:12px;font-family:Arial;width:30%;'>Company</td><td style='width:40%;' ><input type='text' style='width:100%;box-sizing:border-box;'  maxlength='100' name='Company' /></td><td style='width:30%;'></td></tr><tr><td  style='word-break: break-word;text-align:left;font-size:12px;font-family:Arial;width:30%;'>Last Name<span style='color:red;'>*</span></td><td style='width:40%;' ><input type='text' style='width:100%;box-sizing:border-box;'  maxlength='80' name='Last Name' /></td><td style='width:30%;'></td></tr>
 
-	<tr><td colspan='2' style='text-align:center; padding-top:15px;font-size:12px;'>
-		<input style='margin-right: 5px;cursor: pointer;font-size:12px;color:black' id='formsubmit' type='submit' value='Submit' ></input> <input type='reset' name='reset' style='cursor: pointer;font-size:12px;color:black' value='Reset' ></input> </td></tr></table>
-	<script>
- 	  var mndFileds=new Array('Last Name');
- 	  var fldLangVal=new Array('Last Name');
-		var name='';
-		var email='';
+try {
+    require_once('./class.phpmailer.php');
+    if(isset($_POST['email'])) {
 
- 	  function checkMandatory3978741000000251006() {
-		for(i=0;i<mndFileds.length;i++) {
-		  var fieldObj=document.forms['WebToLeads3978741000000251006'][mndFileds[i]];
-		  if(fieldObj) {
-			if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length==0) {
-			 if(fieldObj.type =='file')
-				{
-				 alert('Please select a file to upload.');
-				 fieldObj.focus();
-				 return false;
-				}
-			alert(fldLangVal[i] +' cannot be empty.');
-   	   	  	  fieldObj.focus();
-   	   	  	  return false;
-			}  else if(fieldObj.nodeName=='SELECT') {
-  	   	   	 if(fieldObj.options[fieldObj.selectedIndex].value=='-None-') {
-				alert(fldLangVal[i] +' cannot be none.');
-				fieldObj.focus();
-				return false;
-			   }
-			} else if(fieldObj.type =='checkbox'){
- 	 	 	 if(fieldObj.checked == false){
-				alert('Please accept  '+fldLangVal[i]);
-				fieldObj.focus();
-				return false;
-			   }
-			 }
-			 try {
-			     if(fieldObj.name == 'Last Name') {
-				name = fieldObj.value;
- 	 	 	    }
-			} catch (e) {}
-		    }
-		}
-		document.getElementById('formsubmit').disabled=true;
-	}
-</script>
-	</form>
-</div>
+        $email = $_POST['email'];
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        $company = $_POST['company'];
+        $phone = $_POST['phone'];
+        $subject = $name . " contacting you via Equilibriumict.com";
+
+
+        $error_message = "";
+        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+        $string_exp = "/^[A-Za-z .'-]+$/";
+
+
+            if(!array_key_exists('gotcha', $_POST)) {
+            died("Who do you think you are trying to abuse us.");
+        }
+
+        if(!preg_match($email_exp, $email)) {
+            $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+        }
+
+        if(!preg_match($string_exp,$name)) {
+            $error_message .= 'The Name you entered does not appear to be valid.<br />';
+        }
+
+        if(strlen($error_message) > 0) {
+            died($error_message);
+        }
+
+        $html_body = "Name: " . $name . "<br>" . "Email: " . $email . "<br>" . "Company: " . $company . "<br>" . "Phone: " . $phone . "<br>" . "Message: " . $message;
+
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host       = "smtp.office365.com";
+        $mail->Port       = 587;
+        $mail->SMTPSecure = "tls";
+        $mail->SMTPAuth   = true;
+        $mail->Username   = "help@elpatronit.com";
+        $mail->Password   = getenv("EMAIL_PASSWORD");
+        $mail->SetFrom("help@elpatronit.com", "FromEmail");
+        $mail->AddAddress("info@equilibriumict.com", "ToEmail");
+        $mail->IsHTML(true);
+        $mail->Subject    = $subject;
+        $mail->MsgHTML($html_body);
+        if(!$mail->send()) {
+            $error_message .= 'Mailer Error: ' . $mail->ErrorInfo;
+            died($error_message);
+        }
+        header("Location:http://equilibriumict.com/#contact_success");
+        exit();
+        }
+    else{
+        echo "No email found in POST" . $_POST;
+    }
 }
 catch (Exception $e)
 {
